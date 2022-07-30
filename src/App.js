f75a876106dbad1d11233d1e5fe0189d8fa9cab5
Zahra Nejati <nejatizahra4@gmail.com>
2022-07-30 12:07:28 +0430

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const BUY_CAKE = "BUY_CAKE";
+
+  //action creator
+  function buyCake() {
+    return {
+      type: BUY_CAKE,
+    };
+  }
+
+  const initialState = {
+    numOfCake: 10,
+  };
+
+  const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case BUY_CAKE:
+        return { ...state, numOfCake: numOfCake - 1 };
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">Hello world</div>;<h1>hello world</h1>
+    </>
   );
 }
 
